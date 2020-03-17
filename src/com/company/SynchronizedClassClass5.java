b6be24afa74e030a9e951550e894b83280be79cd
spring <aaa10080@qq.com>
2020-03-17 09:34:45 +0800

    init

---
+++ src/com/company/SynchronizedClassClass5.java
@@ -0,0 +1,34 @@
+package com.company;
+
+
+public class SynchronizedClassClass5 implements Runnable {
+    static SynchronizedClassClass5 instance1 = new SynchronizedClassClass5();
+    static SynchronizedClassClass5 instance2 = new SynchronizedClassClass5();
+    @Override
+    public void run() {
+        method();
+    }
+
+    private void method(){
+        synchronized (SynchronizedClassClass5.class){
+            System.out.println("我是类锁的第二种形式：synchronized(*.class),我叫"+Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "执行完毕。");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(instance1);
+        Thread t2 = new Thread(instance2);
+        t1.start();
+        t2.start();
+        while(t1.isAlive() || t2.isAlive()){
+
+        }
+        System.out.println("finished");
+    }
+}
